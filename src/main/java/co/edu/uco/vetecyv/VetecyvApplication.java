5142e201e6a0576ca2de68e84eeb64218a06cb1f
Claroso <claryrivillaas@gmail.com>
2025-10-07 17:45:19 -0500

    primer commit

---
+++ src/main/java/co/edu/uco/vetecyv/VetecyvApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.vetecyv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetecyvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetecyvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/vetecyv/vetecyv/VetecyvApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.vetecyv.vetecyv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetecyvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
